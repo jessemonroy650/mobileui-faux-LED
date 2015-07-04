8fe232e8ec334f4e49d4aed0af9681d08bc0cd5e
Jesse Monroy <jessemonroy650@yahoo.com>
2015-07-04 15:00:20 -0700

    update documentation, variable and function names, and formatting.

--- README.md
+++ README.md
@@ -7,7 +7,18 @@ Three LEDS (red, green, &amp; blue) that timed rotation or manual rotation.
 * css/mobile-faux-leds.css
 * js/mobile-faux-leds.js
 
+**Required HTML**
+
+```
+<div id=full_button class="boxy wrapper">
+    <div id=LED_1 class="circle">&nbsp;</div>
+    <div id=LED_2 class="circle">&nbsp;</div>
+    <div id=LED_3 class="circle">&nbsp;</div>
+</div>
+```
+
 **Functions***
 
 * LEDS.timedRotate(ms); - timed rotation, default to 1000ms.
 * LEDS.rotate(); - manual rotation.
+
--- js/mobileui-faux-leds.js
+++ js/mobileui-faux-leds.js
@@ -3,14 +3,14 @@
 //
 var LEDS = {
 
-    myvar : 0,
-    mytime : 1000,
+    timeHandle : 0,
+    timeOut    : 1000,
 
     init : function () {
         $("#LED_1").addClass("red");
     },
 
-    end : function () {
+    clear : function () {
         $("#LED_1").removeClass("red");
         $("#LED_2").removeClass("green");
         $("#LED_3").removeClass("blue");
@@ -18,15 +18,15 @@ var LEDS = {
 
     timedRotate : function(timeInterval) {
         if (timeInterval) {
-            LEDS.mytime = timeInterval;
+            LEDS.timeOut = timeInterval;
         }
-        if (LEDS.myvar) {
+        if (LEDS.timeHandle) {
             console.log("clicked off.");
-            clearInterval(LEDS.myvar);
-            LEDS.end();
+            clearInterval(LEDS.timeHandle);
+            LEDS.clear();
         } else {
             LEDS.init();
-            LEDS.myvar = setInterval(LEDS.rotate, LEDS.mytime);
+            LEDS.timeHandle = setInterval(LEDS.rotate, LEDS.timeOut);
             console.log("clicked on.");
         }
     },
@@ -35,17 +35,12 @@ var LEDS = {
         if ($("#LED_1").hasClass("red")) {
             $("#LED_1").toggleClass("red");
             $("#LED_2").addClass("green");
-            return;
-        }
-        if ($("#LED_2").hasClass("green")) {
+        } else if ($("#LED_2").hasClass("green")) {
             $("#LED_2").toggleClass("green");
             $("#LED_3").addClass("blue");
-            return;
-        }
-        if ($("#LED_3").hasClass("blue")) {
+        } else if ($("#LED_3").hasClass("blue")) {
             $("#LED_3").toggleClass("blue");
             $("#LED_1").addClass("red");
-            return;
         }
     }
 };
